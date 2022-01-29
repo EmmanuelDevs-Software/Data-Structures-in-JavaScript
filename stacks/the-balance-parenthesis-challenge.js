d0bf92e543031699c28f1884ee92f1f816dc4a46
Emmanuel Davila <emmanuel.davila@altran.com>
2022-01-29 20:00:17 +0100

    linked list and stacks

---
+++ stacks/the-balance-parenthesis-challenge.js
@@ -0,0 +1,36 @@
+function peek(stack){
+  return stack[stack.length - 1]
+}
+
+function isBalanced(str){
+  let stack  = [];
+
+  // iterate through every lleter of the string
+  for (var i = 0; i < str.length; i++) {
+    let letter = str.charAt(i)
+    // if the letter is an opening paren push it on the stack
+    if(letter === '('){
+      stack.push(letter)
+    }else if(letter  === ')'){
+        // if the letter is a closing paren make sure it has a matching opening paren
+      if(peek(stack) === '('){
+        stack.pop()
+      }else{
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0
+}
+console.log('TRUE CASES')
+console.log(isBalanced('()'))
+console.log(isBalanced('(())'))
+console.log(isBalanced('((()))'))
+console.log(isBalanced('(((())))'))
+console.log('TRUE FALSE')
+console.log(isBalanced('(((()))'))
+console.log(isBalanced('('))
+console.log(isBalanced(')'))
+console.log(isBalanced(')('))
+console.log(isBalanced(')()'))
